08c951fa25706924a32e345e1fc64a0cd13d8c67
2050189 <130997178+2050189@users.noreply.github.com>
2024-09-14 12:17:08 -0400

    update resume

--- src/app/components/hello/hello.component.jsx
+++ src/app/components/hello/hello.component.jsx
@@ -9,7 +9,8 @@ const Hello = () => {
             <p className="section__description">
                 I am currently pursuing a degree in Computer Engineering at Concordia University, where I have developed a solid foundation in both programming and hardware design. I am interested in an internship that offers the opportunity to work on projects involving both software development and hardware design.
             </p>
-            <a href="/Resume.pdf" target="_blank" className="btn"><FontAwesomeIcon icon={faDownload} /> Download CV</a>
+            <a href="/Resumé_English.pdf" target="_blank" className="btn mr-3"><FontAwesomeIcon icon={faDownload} /> Download CV (EN)</a>
+            <a href="/Résumé_French.pdf" target="_blank" className="btn"><FontAwesomeIcon icon={faDownload} /> Download CV (FR)</a>
         </section>
     </>);
 }
